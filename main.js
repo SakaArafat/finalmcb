f74cff1f829cd7efed93f822f9c36bc1de60c707
Olakunle <kunleoladimeji@gmail.com>
2017-05-11 22:59:28 +0400

    mods

--- main.js
+++ main.js
@@ -5,7 +5,10 @@
  */
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
+const watson = require("watson-developer-cloud/conversation/v1")
 const app = express();
+var contexts = [];
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
@@ -33,7 +36,78 @@ app.post('/webhook', (req, res) => {
 })
 app.get("/test", (req, res) => {
     res.json("Hello There, this app works!");
-})
+});
+
+function sendMessage(event) {
+    let sender = event.sender.id;
+    let message = event.message.text;
+    let context = null;
+    let contextIndex = 0;
+    let index = 0;
+    contexts.forEach((value) => {
+        console.log(value.from)
+        if (value.from == sender) {
+            context = value.context;
+            contextIndex = index;
+        }
+        index++;
+    });
+    console.log("Received message from " + sender + " saying '" + message + "'");
+    var conversation = new watson({
+        username: "bdce6430-3faa-4ee8-a5db-8cb521efd395",
+        password: "te7tJKBhISig",
+        version: watson.VERSION_DATE_2016_09_20
+    });
+    console.log(JSON.stringify(context));
+    console.log(contexts.length);
+    conversation.message({
+        input: {
+            text: message
+        },
+        workspace_id: "cb34787f-638d-40d8-99bf-4ddbfa893732",
+        context: context,
+    }, (err, response) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(response.output.text[0]);
+            if (!context) {
+                contexts.push({
+                    "from": sender,
+                    "context": response.context
+                })
+            } else {
+                contexts[contextIndex].context = response.context;
+            }
+            var intent = response.intents[0].intent;
+            console.log(intent);
+            if (intent == 'done') {
+                contexts.splice(contextIndex, 1);
+            }
+            request({
+                url: "https://graph.facebook.com/v2.6/me/messages",
+                qs: {
+                    access_token: "EAAEhpuUYLXkBAKatG6W5vF04vNwDMUBMOrXIbeNTVOMTRZBaj9ZCXDoQDxZBiKVuywKrGlfQUlJstO2YRqrarE6Tqr3ZCXmkOTUZAfflK5i2ccJ7wFaSaMTr7AJw0ersvUPo3Q09HhqVuM0NNbdmxcSirQH7Fv3oe5bTTdem5VgZDZD"
+                },
+                method: "POST",
+                json: {
+                    recipient: {
+                        id: sender
+                    },
+                    message: {
+                        text: response.output.text[0]
+                    }
+                }
+            }, (error, response) => {
+                if (error) {
+                    console.log("Error sending message", error);
+                } else if (response.body.error) {
+                    console.log("Error: ", response.body.error);
+                }
+            });
+        }
+    });
+}
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log("Server starting on port: " + server.address().port)
 })
